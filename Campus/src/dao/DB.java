7ff5545228bcdc612bceecff3e2adcad2ca4c992
Macren <Madeleine@172.16.12.14>
2014-03-24 10:04:35 +0100

    implement Database class

--- Campus/src/dao/DB.java
+++ Campus/src/dao/DB.java
@@ -6,10 +6,40 @@
 
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Thierry
  */
 public class DB {
     
+    private Connection  conn;
+    private String      user = "root";
+    private String      passwd = "";
+    private String      url = "jdbc:mysql://localhost/campus";
+    
+    public Connection   connect() throws ClassNotFoundException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            this.conn = (Connection) DriverManager.getConnection(url, user, passwd);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return this.conn;
+    }
+    
+    public void disconnect(Connection connection)
+    {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
