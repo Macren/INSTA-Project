624139966d1081182815fbb6ff91fcba26139450
Tndongala <blakozai92@hotmail.fr>
2014-03-24 13:56:26 +0100

    Implementation des classes métiers suivant et ajout de la bibliothèque MySql : -Discipline -Education -Lesson -LessonStatus -School

--- Campus/src/metier/Lesson.java
+++ Campus/src/metier/Lesson.java
@@ -1,15 +1,153 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package metier;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  *
  * @author Thierry
  */
-public class Lesson {
+public class Lesson implements Serializable {
+   
+    /**
+     * Variables
+     */
+    private int             id;
+    private String          name;
+    private boolean         tp;
+    private boolean         test;
+    private Calendar        beginHour;
+    private Calendar        endHour;
+    private LessonStatus    status;
+    private Teacher         teacher;
+    private List<Student>   students;
+    
+    /**
+     * Constructeur
+     * @param id
+     * @param name
+     * @param tp
+     * @param test
+     * @param beginHour
+     * @param endHour
+     * @param status
+     * @param teacher
+     * @param students 
+     */
+    public Lesson(int id, String name, boolean tp, boolean test, Calendar beginHour, Calendar endHour, CourseStatus status, Teacher teacher, List<Student> students) {
+        this.id = id;
+        this.name = name;
+        this.tp = tp;
+        this.test = test;
+        this.beginHour = beginHour;
+        this.endHour = endHour;
+        this.status = status;
+        this.teacher = teacher;
+        this.students = students;
+    }
+    /**
+     * Constructeur sans id
+     * @param name
+     * @param tp
+     * @param test
+     * @param beginHour
+     * @param endHour
+     * @param status
+     * @param teacher
+     * @param students 
+     */
+    public Lesson(String name, boolean tp, boolean test, Calendar beginHour, Calendar endHour, CourseStatus status, Teacher teacher, List<Student> students) {
+        this.name = name;
+        this.tp = tp;
+        this.test = test;
+        this.beginHour = beginHour;
+        this.endHour = endHour;
+        this.status = status;
+        this.teacher = teacher;
+        this.students = students;
+    }
+    
+    /**
+     * Getters setters
+     * @return 
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isTp() {
+        return true;
+    }
+
+    public void setTp(boolean tp) {
+        this.tp = tp;
+    }
+
+    public boolean isTest() {
+        return true;
+    }
+
+    public void setTest(boolean test) {
+        this.test = test;
+    }
+
+    public Calendar getBeginHour() {
+        return beginHour;
+    }
+
+    public void setBeginHour(Calendar beginHour) {
+        this.beginHour = beginHour;
+    }
+
+    public Calendar getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(Calendar endHour) {
+        this.endHour = endHour;
+    }
+
+    public CourseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CourseStatus status) {
+        this.status = status;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson{" + "name=" + name + ", tp=" + tp + ", test=" + test + ", beginHour=" + beginHour + ", endHour=" + endHour + ", status=" + status + ", teacher=" + teacher + ", students=" + students + '}';
+    }
+    
     
 }
--- Campus/src/metier/School.java
+++ Campus/src/metier/School.java
@@ -6,10 +6,91 @@
 
 package metier;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  *
  * @author Thierry
  */
-public class School {
+public class School implements Serializable {
+   
+    /**
+     * Attributs
+     */
+    private int             id;
+    private String          name;  
+    private List<Teacher>   teacher;
+    private List<Education> educations;
+    
+    /**
+     * Constructeur
+     * @param id
+     * @param name
+     * @param teacher
+     * @param educations 
+     */
+    public School(int id, String name, List<Teacher> teacher, List<Education> educations) {
+        this.id = id;
+        this.name = name;
+        this.teacher = teacher;
+        this.educations = educations;
+    }
+    /**
+     * Constructeur sans id
+     * @param name
+     * @param teacher
+     * @param educations 
+     */
+    public School(String name, List<Teacher> teacher, List<Education> educations) {
+        this.name = name;
+        this.teacher = teacher;
+        this.educations = educations;
+    }
+    
+    /**
+     * Getters setters
+     * @return 
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Teacher> getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(List<Teacher> teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Education> getEducations() {
+        return educations;
+    }
+
+    public void setEducations(List<Education> educations) {
+        this.educations = educations;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" + "name=" + name + ", teacher=" + teacher + ", educations=" + educations + '}';
+    }
+    
+    
+    
+    
     
 }
