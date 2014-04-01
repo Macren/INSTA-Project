f20e72749a2c67ac913cb67a3a2e1745d06c2232
biron75 <biron75@gmail.com>
2014-04-01 17:43:50 +0200

    Ajout fonctionnalité Ftp + tests

--- Campus/src/utils/FtpUtils.java
+++ Campus/src/utils/FtpUtils.java
@@ -8,12 +8,15 @@ package utils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPHTTPClient;
 
 
 /**
@@ -22,29 +25,36 @@ import org.apache.commons.net.ftp.FTPFile;
  */
 public class FtpUtils {
   
+  private static final String PROXY_HOST = "172.16.13.3";
+  private static final int PROXY_PORT = 8080;
+  private static final String PROXY_USER = "d.gauthier";
+  private static final String PROXY_PWD = "Bbfbxh16";
+  
   private static final String URL_FTP = "www.ok-team.com";
   private static final String USER_FTP = "okteamco";
   private static final String PWD_FTP = "6Hklq718eD";
+  private static final String PATH_DOSSIER = "/campus_project/";
   
-  private FTPClient ftpClient;
+  private static final String PATH_DOSSIER_LOCAL = "./";
   
+  private static FTPClient ftpClient = new FTPHTTPClient(PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PWD);
   
-  public String[] browseFileNamesOnFtp(){
+  public static String[] browseFileNamesOnFtp(){
     
     String[] fileNames = null;
     
     try {
-      this.ftpClient.connect(URL_FTP);
-      this.ftpClient.login(USER_FTP, PWD_FTP);
+      ftpClient.connect(URL_FTP);
+      ftpClient.login(USER_FTP, PWD_FTP);
+      ftpClient.enterLocalPassiveMode();
+      ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
       
       // Obtain a list of filenames in the current working directory.
       // When no file found an empty array will be returned.
-      fileNames = this.ftpClient.listNames();
-      for (String name : fileNames) {
-          System.out.println("Name = " + name);
-      }
+      fileNames = ftpClient.listNames(PATH_DOSSIER);
+      //fileNames = ftpClient.listNames();
       
-      this.ftpClient.logout();
+      ftpClient.logout();
       return fileNames;
       
     } catch (IOException ex) {
@@ -52,7 +62,7 @@ public class FtpUtils {
     } finally {
       try
       {
-        this.ftpClient.disconnect();
+        ftpClient.disconnect();
       } catch (IOException e) {
         e.printStackTrace();
       }
@@ -62,29 +72,32 @@ public class FtpUtils {
   
   
   
-  public boolean uploadFileOnFtp(String pFileName, File pFile){
+  public static boolean uploadFileOnFtp(String pFileName, File pFile){
     
     boolean retour = false;
     
     try {
-      this.ftpClient.connect(URL_FTP);
-      this.ftpClient.login(USER_FTP, PWD_FTP);
+      ftpClient.connect(URL_FTP);
+      ftpClient.login(USER_FTP, PWD_FTP);
+      ftpClient.enterLocalPassiveMode();
+      ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
       
       // Create an InputStream of the file to be uploaded
-      String filename = pFileName;
+      String filename = PATH_DOSSIER + pFileName;
       FileInputStream fis = new FileInputStream(pFile);
       
       // Store file to server
-      retour = this.ftpClient.storeFile(filename, fis);
+      retour = ftpClient.storeFile(filename, fis);
       
-      this.ftpClient.logout();
+      ftpClient.logout();
+      return retour;
       
     } catch (IOException ex) {
       Logger.getLogger(FtpUtils.class.getName()).log(Level.SEVERE, null, ex);
     } finally {
       try
       {
-        this.ftpClient.disconnect();
+        ftpClient.disconnect();
       } catch (IOException e) {
         e.printStackTrace();
       }
@@ -97,9 +110,83 @@ public class FtpUtils {
   
   
   
-  public boolean downloadFromFtp(){
-    return false;
+  public static boolean deleteFileOnFtp(String pFileName){
+    
+    boolean retour = false;
+    
+    try {
+      ftpClient.connect(URL_FTP);
+      ftpClient.login(USER_FTP, PWD_FTP);
+      ftpClient.enterLocalPassiveMode();
+      ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+      
+      // Delete file on the FTP server. When the FTP delete complete 
+      // it returns true.
+      String filename = PATH_DOSSIER + pFileName;
+      retour = ftpClient.deleteFile(filename);
+      
+      ftpClient.logout();
+      return retour;
+      
+    } catch (IOException ex) {
+      Logger.getLogger(FtpUtils.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      try
+      {
+        ftpClient.disconnect();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    
+    return retour;
   }
+  
+  
+  
+  
+  
+  public static boolean downloadFromFtp(String pFileName){
+    
+    boolean retour = false;
+    FileOutputStream fos = null;
+    
+    try {
+      ftpClient.connect(URL_FTP);
+      ftpClient.login(USER_FTP, PWD_FTP);
+      ftpClient.enterLocalPassiveMode();
+      ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+      
+      // The remote filename to be downloaded.
+      String filename = PATH_DOSSIER + pFileName;
+      fos = new FileOutputStream(PATH_DOSSIER_LOCAL + "remoteFiles/" + pFileName);
+      
+      // Download file from FTP server
+      retour = ftpClient.retrieveFile(filename, fos);
+//      this.ftpClient.logout();
+      return retour;
+      
+    } catch (IOException ex) {
+      Logger.getLogger(FtpUtils.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      try {
+        if (fos != null) {
+          fos.close();
+        }
+        ftpClient.disconnect();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    
+    return retour;
+  }
+  
+  
+  
+  
+  
+  
   
   
 }
---
+++ Campus/test/utils/FtpUtilsTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author biron
+ */
+public class FtpUtilsTest {
+  
+  public FtpUtilsTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+  
+  @AfterClass
+  public static void tearDownClass() {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
+
+  /**
+   * Test of browseFileNamesOnFtp method, of class FtpUtils.
+   */
+  @Test
+  public void testBrowseFileNamesOnFtp() {
+    boolean result = FtpUtils.browseFileNamesOnFtp().length > 0;
+    assertTrue(result);
+  }
+
+  /**
+   * Test of uploadFileOnFtp method, of class FtpUtils.
+   */
+  @Test
+  public void testUploadFileOnFtp() {
+    boolean result = FtpUtils.uploadFileOnFtp("toto.png", new File("/home/biron/Desktop/icone_boss2.png"));
+    assertTrue(result);
+  }
+
+  /**
+   * Test of deleteFileOnFtp method, of class FtpUtils.
+   */
+  @Test
+  public void testDeleteFileOnFtp() {
+    boolean result = FtpUtils.deleteFileOnFtp("toto.png");
+    assertTrue(result);
+  }
+
+  /**
+   * Test of downloadFromFtp method, of class FtpUtils.
+   */
+  @Test
+  public void testDownloadFromFtp() {
+    boolean result = FtpUtils.downloadFromFtp("icone_boss.png");
+    assertTrue(result);
+  }
+  
+}
